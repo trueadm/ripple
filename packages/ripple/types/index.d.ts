cc9bc0428825868224dbc01287a798ec8ae6d057
Dominic Gannaway <dg@domgan.com>
2025-08-28 23:01:51 +0100

    fix types

--- packages/ripple/types/index.d.ts
+++ packages/ripple/types/index.d.ts
@@ -1,2 +1,8 @@
-
 export type Fragment<T extends any[] = []> = (...args: T) => void;
+
+export declare function mount(
+	component: () => void,
+	options: { target: HTMLElement; props?: Record<string, any> }
+): () => void;
+
+export declare function untrack<T>(fn: () => T): T;
