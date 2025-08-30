4ef06612df70404ea95bdc9599a8aae8140ca94d
Moustapha <hello@moustapha.dev>
2025-08-30 12:05:39 +0000

    add overview page and syntax highlight

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,25 +2,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const grammarPath = path.resolve(
+	__dirname,
+	'../ripple-vscode-plugin/syntaxes/ripple.tmLanguage.json'
+);
+const rippleGrammar = JSON.parse(fs.readFileSync(grammarPath, 'utf8'));
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Ripple',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/trueadm/ripple' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Introduction',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' }
+						{ label: 'Overview', slug: 'intro/overview' },
+						{ label: 'Getting started', slug: 'intro/getting-started' }
 					]
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' }
 				}
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' }
+				// }
 			]
 		})
-	]
+	],
+	markdown: {
+		shikiConfig: {
+			langs: [{ ...rippleGrammar, name: 'ripple' }]
+		}
+	}
 });
---
+++ packages/website/src/content/docs/intro/getting-started.md
@@ -0,0 +1,10 @@
+---
+title: Getting started
+description: A guide in my new Starlight docs site.
+---
+
+pnpm asddd
+
+## Editor setup
+
+## Getting help
