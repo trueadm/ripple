f80c4b8a3daac619ba158545ea5272d5757e608c
Leonidas <329182+leonidaz@users.noreply.github.com>
2025-09-07 17:39:34 -0400

    feedback

--- packages/ripple/src/runtime/internal/client/runtime.js
+++ packages/ripple/src/runtime/internal/client/runtime.js
@@ -1048,16 +1048,23 @@ export function with_scope(block, fn) {
 	}
 }
 
+/**
+ * @returns {Block | null}
+ */
 export function scope() {
 	return active_scope;
 }
 
+/**
+ * @param {string} [err]
+ * @returns {Block | never}
+ */
 export function safe_scope(err = 'Cannot access outside of a component context') {
 	if (active_scope === null) {
 		throw new Error(err);
 	}
 
-	return scope();
+	return /** @type {Block} */ (active_block);
 }
 
 export function push_component() {
