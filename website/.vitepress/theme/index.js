8db5bb9eedff8ec2b6fee5a60d83f5bc15c1e1a9
JakeIsMeh <jakeismeh1@gmail.com>
2025-09-13 08:34:47 +0800

    add warning about documentation quality

--- website/.vitepress/theme/index.js
+++ website/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import './styles.css'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import CustomLayout from './CustomLayout.vue'
 
 export default {
 	extends: DefaultTheme,
+	Layout: CustomLayout,
 	enhanceApp({ app }) {
 		enhanceAppWithTabs(app)
 	}
