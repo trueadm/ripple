528b2510e51812a72d0cfb083704e41f41310763
JakeIsMeh <jakeismeh1@gmail.com>
2025-09-13 08:34:47 +0800

    add warning about documentation quality

---
+++ website/.vitepress/theme/CustomLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+	<Layout>
+		<template #doc-before>
+			<div class="warning custom-block">
+				<span>
+					Most of the docs are currently bulk imported from llms.txt.
+					Expect terse and incomplete information!
+				</span>
+			</div>
+			<br>
+		</template>
+	</Layout>
+</template>
