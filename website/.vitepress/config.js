649b8246ae99b3527614343d96efe6842c60b2af
RATIU5 <jcmemmott20@outlook.com>
2025-09-22 15:46:36 -0600

    update vitepress docs to use ico format favicon

--- website/.vitepress/config.js
+++ website/.vitepress/config.js
@@ -80,7 +80,7 @@ export default defineConfig({
 	},
 
 	head: [
-		['link', { rel: 'icon', href: '/favicon.svg' }],
+		['link', { rel: 'icon', href: '/favicon.ico' }],
 		[
 			'link',
 			{ rel: 'preconnect', href: 'https://fonts.googleapis.com' }
