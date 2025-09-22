608f00844a0618e2d6e5d04e8d45403683e912f8
RATIU5 <jcmemmott20@outlook.com>
2025-09-22 14:54:17 -0600

    update: favicon on website & docs

--- website/.vitepress/config.js
+++ website/.vitepress/config.js
@@ -80,7 +80,7 @@ export default defineConfig({
 	},
 
 	head: [
-		['link', { rel: 'icon', href: '/favicon.ico' }],
+		['link', { rel: 'icon', href: '/favicon.svg' }],
 		[
 			'link',
 			{ rel: 'preconnect', href: 'https://fonts.googleapis.com' }
