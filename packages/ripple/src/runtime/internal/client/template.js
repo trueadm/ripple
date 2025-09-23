d64d85f34ec7717c4bcb786cd85f2c424d17c30f
anubra266 <anubra266@gmail.com>
2025-09-23 02:16:02 -0700

    Fix svg namespace

--- packages/ripple/src/runtime/internal/client/template.js
+++ packages/ripple/src/runtime/internal/client/template.js
@@ -13,6 +13,10 @@ export function assign_nodes(start, end) {
 }
 
 function create_fragment_from_html(html) {
+  if (is_svg_template(html)) {
+    return create_svg_fragment_from_html(html);
+  }
+
   var elem = document.createElement('template');
   elem.innerHTML = html;
   return elem.content;
@@ -48,4 +52,88 @@ export function template(content, flags) {
 
 export function append(anchor, dom) {
   anchor.before(/** @type {Node} */ (dom));
+}
+
+const SVG_ELEMENTS = new Set([
+  'svg',
+  'g',
+  'defs',
+  'symbol',
+  'use',
+  'switch',
+  'rect',
+  'circle',
+  'ellipse',
+  'line',
+  'polyline',
+  'polygon',
+  'path',
+  'text',
+  'tspan',
+  'textPath',
+  'linearGradient',
+  'radialGradient',
+  'pattern',
+  'stop',
+  'clipPath',
+  'mask',
+  'filter',
+  'feBlend',
+  'feColorMatrix',
+  'feComponentTransfer',
+  'feComposite',
+  'feConvolveMatrix',
+  'feDiffuseLighting',
+  'feDisplacementMap',
+  'feDistantLight',
+  'feDropShadow',
+  'feFlood',
+  'feFuncA',
+  'feFuncR',
+  'feFuncG',
+  'feFuncB',
+  'feGaussianBlur',
+  'feImage',
+  'feMerge',
+  'feMergeNode',
+  'feMorphology',
+  'feOffset',
+  'fePointLight',
+  'feSpecularLighting',
+  'feSpotLight',
+  'feTile',
+  'feTurbulence',
+  'image',
+  'foreignObject',
+  'a',
+  'view',
+  'animate',
+  'animateMotion',
+  'animateTransform',
+  'set',
+  'mpath',
+  'title',
+  'desc',
+  'metadata',
+  'marker',
+  'script',
+  'style',
+]);
+
+function is_svg_template(html) {
+  const match = html.match(/<(\w+)/);
+  return match && SVG_ELEMENTS.has(match[1].toLowerCase());
+}
+
+const SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
+
+function create_svg_fragment_from_html(html) {
+  var svgContainer = document.createElementNS(SVG_NAMESPACE, 'svg');
+  svgContainer.innerHTML = html;
+
+  var fragment = document.createDocumentFragment();
+  while (svgContainer.firstChild) {
+    fragment.appendChild(svgContainer.firstChild);
+  }
+  return fragment;
 }
