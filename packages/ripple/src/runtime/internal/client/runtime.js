b51e7cd3576c70e238e2f4618d924e430af91cec
Leonidas <329182+leonidaz@users.noreply.github.com>
2025-09-24 15:40:31 -0400

    Adds support for track props split (#286)
    * add support for track split of props
    
    * jsx to ripple
    
    * monomorphic track, docs
    
    * feedback
    
    * dont delete

--- packages/ripple/src/runtime/internal/client/runtime.js
+++ packages/ripple/src/runtime/internal/client/runtime.js
@@ -1,4 +1,5 @@
 /** @import { Block, Component, Dependency, Derived, Tracked } from '#client' */
+/** @import { TrackOptions } from '#public' */
 
 import {
   destroy_block,
@@ -26,7 +27,7 @@ import {
   REF_PROP,
 } from './constants.js';
 import { capture, suspend } from './try.js';
-import { define_property, is_tracked_object } from './utils.js';
+import { define_property, get_descriptors, is_array, is_tracked_object } from './utils.js';
 
 const FLUSH_MICROTASK = 0;
 const FLUSH_SYNC = 1;
@@ -276,6 +277,65 @@ export function derived(fn, block) {
     fn,
     v: UNINITIALIZED,
   };
+}
+
+/**
+ * @param {any} v
+ * @param {TrackOptions | undefined} o
+ * @param {Block} b
+ * @returns {Tracked | Derived | Tracked[]}
+ */
+export function track(v, o, b) {
+  var is_tracked = is_tracked_object(v);
+
+  if (o === undefined) {
+    if (is_tracked) {
+      return v;
+    }
+
+    if (typeof v === 'function') {
+      return derived(v, b);
+    }
+    return tracked(v, b);
+  }
+
+  if (is_tracked || typeof v !== "object" || v === null || is_array(v)) {
+    throw new TypeError('Invalid value: expected a non-tracked object');
+  }
+
+  var list = o.split ?? [];
+  /** @type {Tracked[]} */
+  var out = [];
+  /** @type {Record<string|symbol, any>} */
+  var rest = {};
+  /** @type {Record<PropertyKey, any | null>} */
+  var descriptors = get_descriptors(v);
+
+  for (let i = 0, key, t; i < list.length; i++) {
+    key = list[i];
+
+    if (is_tracked_object(v[key])) {
+      t = v[key];
+    } else {
+      t = define_property(tracked(undefined, b), 'v', descriptors[key]);
+    }
+
+    out[i] = t;
+    descriptors[key] = null;
+  }
+
+  var props = Reflect.ownKeys(descriptors);
+  for (let i = 0, key; i < props.length; i++) {
+    key = props[i];
+    if (descriptors[key] === null) {
+      continue;
+    }
+    define_property(rest, key, descriptors[key]);
+  }
+
+  out.push(tracked(rest, b));
+
+  return out;
 }
 
 /**
