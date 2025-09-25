239390637a8717d7c7614e9649d08f8fc68548e2
Hrutav Modha <modhahrutav@gmail.com>
2025-09-25 19:59:11 +0530

    Removed the temporary created files

--- my-app/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { mount } from 'ripple';
-// @ts-expect-error: known issue, we're working on it
-import { App } from './App.ripple';
-
-mount(App, {
-	target: document.getElementById('root'),
-});
