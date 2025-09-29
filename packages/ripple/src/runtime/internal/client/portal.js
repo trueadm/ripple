5725af426f31a4a09c70224c912f057a2053eb3d
Dominic Gannaway <dg@domgan.com>
2025-09-29 23:41:21 +0100

    fix component signatute

--- packages/ripple/src/runtime/internal/client/portal.js
+++ packages/ripple/src/runtime/internal/client/portal.js
@@ -4,57 +4,63 @@ import { branch, destroy_block, remove_block_dom, render } from './blocks.js';
 import { UNINITIALIZED } from './constants.js';
 import { handle_root_events } from './events.js';
 import { create_text } from './operations.js';
+import { active_block } from './runtime.js';
 
 /**
- * @param {any} _ 
- * @param {{ target: Element, children: (anchor: Node) => void }} props
+ * @param {any} _
+ * @param {{ target: Element, children: (anchor: Node, props: {}, block: Block) => void }} props
  * @returns {void}
  */
 export function Portal(_, props) {
-  /** @type {Element | symbol} */
-  let target = UNINITIALIZED;
-  /** @type {((anchor: Node) => void) | symbol} */
-  let children = UNINITIALIZED;
-  /** @type {Block | null} */
-  var b = null;
-  /** @type {Text | null} */
-  var anchor = null;
-  /** @type {Node | null} */
-  var dom_start = null;
-  /** @type {Node | null} */
-  var dom_end = null;
+	/** @type {Element | symbol} */
+	let target = UNINITIALIZED;
+	/** @type {((anchor: Node, props: {}, block: Block) => void) | symbol} */
+	let children = UNINITIALIZED;
+	/** @type {Block | null} */
+	var b = null;
+	/** @type {Text | null} */
+	var anchor = null;
+	/** @type {Node | null} */
+	var dom_start = null;
+	/** @type {Node | null} */
+	var dom_end = null;
 
-  render(() => {
-    if (target === (target = props.target)) return;
-    if (children === (children = props.children)) return;
+	render(() => {
+		if (target === (target = props.target)) return;
+		if (children === (children = props.children)) return;
 
-    if (b !== null) {
-      destroy_block(b);
-    }
+		if (b !== null) {
+			destroy_block(b);
+		}
 
-    if (anchor !== null) {
-      anchor.remove();
-    }
+		if (anchor !== null) {
+			anchor.remove();
+		}
 
-    dom_start = dom_end = null;
+		dom_start = dom_end = null;
 
-    anchor = create_text();
-    /** @type {Element} */ (target).append(anchor);
+		anchor = create_text();
+		/** @type {Element} */ (target).append(anchor);
 
-    const cleanup_events = handle_root_events(/** @type {Element} */ (target));
+		const cleanup_events = handle_root_events(/** @type {Element} */ (target));
 
-    // @ts-ignore
-    b = branch(() =>  children(anchor));
+		var block = /** @type {Block} */ (active_block);
 
-    dom_start = b?.s?.start;
-    dom_end = b?.s?.end;
+		b = branch(() => {
+			if (typeof children === 'function') {
+				children(/** @type {Text} */ (anchor), {}, block);
+			}
+		});
 
-    return () => {
-      cleanup_events();
-      /** @type {Text} */ (anchor).remove();
-      if(dom_start && dom_end) {
-        remove_block_dom(dom_start, dom_end);
-      }
-    };
-  });
+		dom_start = b?.s?.start;
+		dom_end = b?.s?.end;
+
+		return () => {
+			cleanup_events();
+			/** @type {Text} */ (anchor).remove();
+			if (dom_start && dom_end) {
+				remove_block_dom(dom_start, dom_end);
+			}
+		};
+	});
 }
