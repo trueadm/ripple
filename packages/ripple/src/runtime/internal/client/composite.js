75f81e84caf138b38470c4612dd39357d55469a5
Abraham <anubra266@gmail.com>
2025-10-06 01:12:01 -0700

    Add dynamic elements (#408)
    * fix perf
    
    * cleanup
    
    * cleanup
    
    * chore

--- packages/ripple/src/runtime/internal/client/composite.js
+++ packages/ripple/src/runtime/internal/client/composite.js
@@ -2,10 +2,12 @@
 
 import { branch, destroy_block, render } from './blocks.js';
 import { COMPOSITE_BLOCK } from './constants.js';
+import { apply_element_spread } from './render';
 import { active_block } from './runtime.js';
 
 /**
- * @param {() => (anchor: Node, props: Record<string, any>, block: Block | null) => void} get_component
+ * @typedef {((anchor: Node, props: Record<string, any>, block: Block | null) => void)} ComponentFunction
+ * @param {() => ComponentFunction | keyof HTMLElementTagNameMap} get_component
  * @param {Node} node
  * @param {Record<string, any>} props
  * @returns {void}
@@ -23,9 +25,39 @@ export function composite(get_component, node, props) {
 			b = null;
 		}
 
-		b = branch(() => {
-			var block = active_block;
-			component(anchor, props, block);
-		});
+		if (typeof component === 'function') {
+			// Handle as regular component
+			b = branch(() => {
+				var block = active_block;
+				/** @type {ComponentFunction} */ (component)(anchor, props, block);
+			});
+		} else {
+			// Custom element
+			b = branch(() => {
+				var block = /** @type {Block} */ (active_block);
+
+				var element = document.createElement(
+					/** @type {keyof HTMLElementTagNameMap} */ (component),
+				);
+				/** @type {ChildNode} */ (anchor).before(element);
+
+				if (block.s === null) {
+					block.s = {
+						start: element,
+						end: element,
+					};
+				}
+
+				const spread_fn = apply_element_spread(element, () => props || {});
+				spread_fn();
+
+				if (typeof props?.children === 'function') {
+					var child_anchor = document.createComment('');
+					element.appendChild(child_anchor);
+
+					props?.children?.(child_anchor, {}, block);
+				}
+			});
+		}
 	}, COMPOSITE_BLOCK);
 }
