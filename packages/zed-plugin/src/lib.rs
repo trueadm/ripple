39064b6b679b87e3a2a3f3b8e7f349371d9b3ccc
Yan Li <Yusyuriv@users.noreply.github.com>
2025-11-07 21:06:57 +0100

    fix: fix editor plugins not working correctly due to language server binary rename

--- packages/zed-plugin/src/lib.rs
+++ packages/zed-plugin/src/lib.rs
@@ -36,14 +36,14 @@ impl RippleExtension {
         let (os, _) = zed::current_platform();
         let candidates: &[&str] = match os {
             zed::Os::Windows => &[
-                "language-server.cmd",
-                "language-server",
-                "node_modules/.bin/language-server.cmd",
-                "node_modules/.bin/language-server",
+                "ripple-language-server.cmd",
+                "ripple-language-server",
+                "node_modules/.bin/ripple-language-server.cmd",
+                "node_modules/.bin/ripple-language-server",
             ],
             _ => &[
-                "@ripple-ts/language-server",
-                "node_modules/.bin/@ripple-ts/language-server",
+                "ripple-language-server",
+                "node_modules/.bin/ripple-language-server",
             ],
         };
 
